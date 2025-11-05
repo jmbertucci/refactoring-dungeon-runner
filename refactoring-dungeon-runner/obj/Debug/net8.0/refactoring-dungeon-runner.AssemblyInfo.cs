4e0aabb80e9f55c3bdc2d2d51b1eef05c6b7c2c0
jbertucci2-adig <jbertucci@gmrmarketing.com>
2025-11-05 11:45:31 -0600

    cleanup git stuff

--- refactoring-dungeon-runner/obj/Debug/net8.0/refactoring-dungeon-runner.AssemblyInfo.cs
+++ refactoring-dungeon-runner/obj/Debug/net8.0/refactoring-dungeon-runner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("refactoring-dungeon-runner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+290d5e4d6cb24479a29f7cc0bc0470c616a71275")]
 [assembly: System.Reflection.AssemblyProductAttribute("refactoring-dungeon-runner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("refactoring-dungeon-runner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
